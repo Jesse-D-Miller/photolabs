216ec8b17168d967d854ed10adbc1fd306ce1428
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-05-14 17:52:43 -0700

    minor chages as per AI suggestion

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,17 +4,17 @@ const PhotoListItem = (props) => {
   const { location, urls, user } = props.sampleDataForPhotoListItem;
 
   return (
-    <div className="PhotoListItem">
-      <div className="photo-list__item">
+    <div className="photo-list__item">
+      <div>
         <img className="photo-list__image" src={urls.regular} alt={`photolabs photo by ${user.username}`} />
       </div>
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={user.profile} alt={`${user.username} profile picture`} />
         <div className="photo-list__user-info" >
           {user.name}
-        </div>
-        <div className="photo-list__user-location ">
-          {`${location.city}, ${location.country}`}
+          <div className="photo-list__user-location ">
+            {`${location.city}, ${location.country}`}
+          </div>
         </div>
       </div>
     </div>
