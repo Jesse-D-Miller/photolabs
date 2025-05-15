7fb0648deb86afaed5e26b7dc64c757dd48b108c
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-05-14 17:46:58 -0700

    photolistitem classNames added

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,23 @@
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
-const { location, urls, user } = props.sampleDataForPhotoListItem;
+  const { location, urls, user } = props.sampleDataForPhotoListItem;
 
   return (
-  <div>
-    <img src={urls.regular} alt={`photolabs photo by ${user.username}`} />
-    <div>
-      <img src={user.profile} alt={`${user.username} profile picture`} />
-      <div>
-        {user.name}
+    <div className="PhotoListItem">
+      <div className="photo-list__item">
+        <img className="photo-list__image" src={urls.regular} alt={`photolabs photo by ${user.username}`} />
       </div>
-      <div>
-        {`${location.city}, ${location.country}`}
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={user.profile} alt={`${user.username} profile picture`} />
+        <div className="photo-list__user-info" >
+          {user.name}
+        </div>
+        <div className="photo-list__user-location ">
+          {`${location.city}, ${location.country}`}
+        </div>
       </div>
     </div>
-  </div> 
   )
 }
 
