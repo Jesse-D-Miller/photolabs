67d8126185f104cd30669ea2b5db8a20750a1bc8
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-05-15 21:38:51 -0700

    the similar photos feature works now, i will have to seek assistance to get the css to match the mockups, i cannot see a way to do it without changing the scss file

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,14 +1,92 @@
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = ({toggleModal, photo}) => {
+const PhotoDetailsModal = ({ toggleModal, photo }) => {
+  const { similar_photos } = photo;
+
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={toggleModal}>
-        <img src={closeSymbol} alt="close symbol" />
-      </button>
-      <img className='photo-details-modal__image' src={photo.urls.full} alt='a really cool photo if something really cool' />
+      <div>
+        <div className="photo-details-modal__top-bar">
+          <button className="photo-details-modal__close-button" onClick={toggleModal}>
+            <img src={closeSymbol} alt="close symbol" />
+          </button>
+        </div>
+        <div >
+          <img
+            className="photo-details-modal__image"
+            src={photo.urls.regular}
+            alt={'a really cool photo of something really cool'}
+          />
+        </div>
+        <div className="photo-details-modal__photographer-details">
+          <img
+            className="photo-details-modal__photographer-profile"
+            src={photo.user.profile}
+            alt={`${photo.user.username} profile picture`}
+          />
+          <div className="photo-details-modal__photographer-info">
+            {photo.user.name}
+            <div className="photo-details-modal__photographer-location">
+              {`${photo.location.city}, ${photo.location.country}`}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="photo-details-modal__images">
+        <h3 className="photo-details-modal__header">Similar Photos</h3>
+        <div className="similar-photos">
+          {Object.values(similar_photos).map((similar_photo) => (
+            <img
+              key={similar_photo.id}
+              src={similar_photo.urls.regular}
+              alt={`similar photo ${similar_photo.id}`}
+              className="photo-details-modal__image"
+            />
+          ))}
+        </div>
+      </div>
     </div>
+
+
+
+
+
+
+
+
+
+
+
+    //       <div className='photo-details-modal__header' >
+    //         <div>
+    //           {/* <PhotoFavButton toggleFavourite={toggleFavourite} favouriteStatus={favouriteStatus} /> */}
+    //           <img className='photo-details-modal__image' src={photo.urls.full} alt='a really cool photo of something really cool' />
+    //         </div>
+    //         <div className="photo-details-modal__top-bar" >
+    //           <img className="photo-details-modal__photographer-profile" src={photo.user.profile} alt={`${photo.user.username} profile picture`} />
+    //           <div className="photo-details-modal__photographer-info" >
+    //             {photo.user.name}
+    //             <div className="photo-details-modal__photographer-location">
+    //               {`${photo.location.city}, ${photo.location.country}`}
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //       <div className='photo-details-modal__images'>
+    //         {Object.values(similar_photos).map((similar_photo) => {
+    //           console.log(similar_photo)
+    //           return (
+    //             < img
+    //               key={similar_photo.id}
+    //               src={similar_photo.urls.regular}
+    //               alt={`similar photo ${similar_photo.id}`}
+    //             />
+    //           )
+    //         })}
+    //       </div>
+    //     </div>
   )
 };
 
