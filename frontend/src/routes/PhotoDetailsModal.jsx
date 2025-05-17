f037de833a7c3db039c02f0188370969e26520d1
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-05-16 18:08:52 -0700

    tyling now matches mockup (mostly)

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,60 +1,32 @@
 import '../styles/PhotoDetailsModal.scss'
+import "../styles/PhotoListItem.scss";
+import "../styles/PhotoList.scss";
 import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoFavButton from '../components/PhotoFavButton';
+import PhotoList from '../components/PhotoList';
 
-const PhotoDetailsModal = ({ toggleModal, photo, toggleFavourite, favourites }) => {
-  const { similar_photos } = photo;
-
-  const dynamicArrayOfSimilarPhotos = Object.values(similar_photos).map((similar_photo) => {
-    const similar_photoId = similar_photo.id;
-    const favouriteStatus = favourites[similar_photoId] || 'notfavourited';
-
-    return (
-      <>
-        <div>
-          <PhotoFavButton toggleFavourite={() => toggleFavourite(similar_photo.id)} favouriteStatus={favouriteStatus || 'notfavourited'} />
-          <img
-            key={photo.id}
-            className='photo-details-modal__image'
-            photo={photo}
-            src={similar_photo.urls.regular}
-            alt={`similar photo ${photo.id}`}
-          />
-        </div>
-     <div className="photo-details-modal__photographer-details">
-          <img
-            className="photo-details-modal__photographer-profile"
-            src={similar_photo.user.profile}
-            alt={`${similar_photo.user.username} profile picture`}
-          />
-          <div className="photo-details-modal__photographer-info">
-            {similar_photo.user.name}
-            <div className="photo-details-modal__photographer-location">
-              {`${similar_photo.location.city}, ${similar_photo.location.country}`}
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  });
-
+const PhotoDetailsModal = (props) => {
+  const { toggleModal, photo, toggleFavourite, favourites } = props;
 
   return (
+    // ----------------------------------->
     <div className="photo-details-modal">
-      <div>
-        <div className="photo-details-modal__top-bar">
-          <button className="photo-details-modal__close-button" onClick={toggleModal}>
-            <img src={closeSymbol} alt="close symbol" />
-          </button>
-        </div>
-        <div >
-          <PhotoFavButton toggleFavourite={() => toggleFavourite(photo.id)} favouriteStatus={favourites[photo.id] || 'notfavourited'} />
-          <img
-            className="photo-details-modal__image"
-            src={photo.urls.regular} //ai says to make this photo.urls.full but its too large
-            alt={'a really cool photo of something really cool'}
-          />
-        </div>
+      <button className="photo-details-modal__close-button" onClick={toggleModal}>
+        <img src={closeSymbol} alt="close symbol" />
+      </button>
+
+      {/*-----------------------------------> matches structure for PhotoListItem */}
+      <div className="photo-details-modal__images">
+
+        <PhotoFavButton toggleFavourite={() => toggleFavourite(photo.id)} favouriteStatus={favourites[photo.id] || 'notfavourited'} />
+
+
+        <img
+          className="photo-details-modal__image"
+          src={photo.urls.regular} //ai says to make this photo.urls.full but its too large
+          alt={'a really cool photo of something really cool'}
+        />
+
         <div className="photo-details-modal__photographer-details">
           <img
             className="photo-details-modal__photographer-profile"
@@ -68,12 +40,16 @@ const PhotoDetailsModal = ({ toggleModal, photo, toggleFavourite, favourites }) 
             </div>
           </div>
         </div>
-      </div>
-      <div className="photo-details-modal__images">
-        <h3 className="photo-details-modal__header">Similar Photos</h3>
-        <div className="similar-photos">
-          {dynamicArrayOfSimilarPhotos}
+
+        {/* -----------------------------------> matches structure for PhotoList */}
+        <div className='photo-details-modal__header' >
+          Related Photos
         </div>
+
+        <div className="photo-details-modal__images">
+          <PhotoList photos={Object.values(props.photo.similar_photos)} favourites={props.favourites} toggleFavourite={props.toggleFavourite} />
+        </div>
+        
       </div>
     </div>
 
